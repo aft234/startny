aa7d65005c25ed300c5dcdd043ebe650be07b256
Anthony Tumbiolo <anthony.tumbiolo@gmail.com>
2014-01-15 00:32:39 -0500

    working on php form

--- mail.php
+++ mail.php
@@ -22,16 +22,17 @@ if ( ! $error) {
 
     // Instantiate the client
     // You can create a free account and get your own API key here: http://www.mailgun.com/
-    $mgClient = new Mailgun('key-3ax6xnjp29jd6fds4gc373sgvjxteol0');
-    $domain = "sandbox45522.mailgun.org";
+    // $mgClient = new Mailgun('key-3ax6xnjp29jd6fds4gc373sgvjxteol0');
+    // $domain = "sandbox45522.mailgun.org";
 
     // Make the call to the client to send email.
-    $result = $mgClient->sendMessage("$domain",
-                          array(
-                                'from'    => "$email",
-                                'to'      => 'start@startny.co',
-                                'subject' => "$type",
-                                ));
+    // $result = $mgClient->sendMessage("$domain",
+    //                       array(
+    //                             'from'    => "$email",
+    //                             'to'      => 'start@startny.co',
+    //                             'subject' => "$type",
+    //                             ));
+    mail("anthony.tumbiolo@gmail.com", "Email Subject", $type, "From: $email" );
 } else {
     // Output any errors
     foreach ($error as $line) {
