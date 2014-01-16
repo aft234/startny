6ca7d2e53fc4b1aa2752206ff569b0271d994481
Joshua Kehn <josh.kehn@gmail.com>
2014-01-16 04:32:15 -0500

    Use direct IP address

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 if [[ $1 == "down" ]]; then
-    rsync -rlv root@startny.byjakt.com:/var/www/jakt/ ./ &&
+    rsync -rlv root@162.242.219.22:/var/www/jakt/ ./ &&
     notify -message "Synced down from startny.co";
 elif [[ $1 == "up" ]]; then
-    rsync -rlzih --owner=startny --group=www.data  --progress --stats --exclude .git ./ root@startny.byjakt.com:/var/www/vhosts/www.startny.co/http_docs/ &&
+    rsync -rlzih --owner=startny --group=www.data  --progress --stats --exclude .git ./ root@162.242.219.22:/var/www/vhosts/www.startny.co/http_docs/ &&
     notify -message "Synced up to startny.co";
 else
     echo "Please specify down or up";
