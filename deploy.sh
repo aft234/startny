3b0e209c0db23fd38b0da0d2b145eb523ca1c616
Joshua Kehn <josh.kehn@gmail.com>
2014-01-16 21:46:36 -0500

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ if [[ $1 == "down" ]]; then
     rsync -rlv root@162.242.219.22:/var/www/jakt/ ./ &&
     notify -message "Synced down from startny.co";
 elif [[ $1 == "up" ]]; then
-    rsync -rlzih --owner=startny --group=www.data  --progress --stats --exclude .git ./ root@162.242.219.22:/var/www/vhosts/www.startny.co/http_docs/ &&
+    rsync -rlzih --owner=startny --group=www-data  --progress --stats --exclude .git ./ root@162.242.219.22:/var/www/vhosts/www.startny.co/http_docs/ &&
     notify -message "Synced up to startny.co";
 else
     echo "Please specify down or up";
